1660c2bd7adf731e520e1d69982f95a8e6b84314
Tobias Tefke <tobias@tefke.name>
2023-11-24 22:37:46 +0100

    Add library for handling shift registers

---
+++ components/suas/suas_ic/74hc595.c
@@ -0,0 +1,93 @@
+#if WITH_SUAS_74HC595
+
+#include <bl_gpio.h>
+#include <bl_timer.h>
+
+#include "74hc595.h"
+
+/*
+    Configure connected shift register(s).
+    Must be called first
+*/
+void configure_74hc595(struct config_74hc595 *config)
+{
+    /* Configure pins as output */
+    if (config->with_output_enable)
+    {
+        bl_gpio_enable_output(config->pin_output_enable, 0, 0);
+    }
+    if (config->with_master_reset)
+    {
+        bl_gpio_enable_output(config->pin_master_reset, 0, 0);
+    }
+
+    bl_gpio_enable_output(config->pin_data_signal, 0, 0);
+    bl_gpio_enable_output(config->pin_shift_clock, 0, 0);
+    bl_gpio_enable_output(config->pin_store_clock, 0, 0);
+
+    /* Set initial values */
+    bl_gpio_output_set(config->pin_data_signal, 0);
+    bl_gpio_output_set(config->pin_shift_clock, 0);
+    bl_gpio_output_set(config->pin_store_clock, 0);
+
+    clear_74hc595(config);
+}
+
+/*
+    Store a byte in the shift register.
+    Only works after configure_74hc595() was called.
+*/
+void store_data_74hc595(struct config_74hc595 *config, uint8_t data)
+{
+    /* Shift out data */
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        /* Extract LSB */
+        uint8_t lsb = (0x1 & (data >> i));
+
+        /* Store bit */
+        bl_gpio_output_set(config->pin_data_signal, lsb);
+        bl_gpio_output_set(config->pin_shift_clock, 1);
+        bl_timer_delay_us(100);
+        bl_gpio_output_set(config->pin_shift_clock, 0);
+    }
+
+    /* Copy data to output register */
+    bl_gpio_output_set(config->pin_store_clock, 1);
+    bl_timer_delay_us(100);
+    bl_gpio_output_set(config->pin_store_clock, 0);
+
+    /* Enable output */
+    if (config->with_output_enable)
+    {
+        bl_gpio_output_set(config->pin_output_enable, 0);
+    }
+}
+
+/*
+    Clear the output of the registers.
+    Only works after configure_74hc595() was called.
+*/
+void clear_74hc595(struct config_74hc595 *config)
+{
+    if (config->with_master_reset)
+    {
+        bl_gpio_output_set(config->pin_master_reset, 0);
+        bl_timer_delay_us(100);
+        bl_gpio_output_set(config->pin_master_reset, 1);
+    }
+    else
+    {
+        for (uint8_t i = 0; i < config->number_of_registers; i++)
+        {
+            store_data_74hc595(config, 0);
+        }
+    }
+
+    if (config->with_output_enable)
+    {
+        bl_gpio_output_set(config->pin_output_enable, 1);
+    }
+}
+
+#endif
---
+++ customer_app/suas_app_shift_register/suas_app_shift_register/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <bl_uart.h>
+#include <hal_hwtimer.h>
+#include <bl_timer.h>
+
+#include <74hc595.h>
+
+void bfl_main(void)
+{
+  /* Initialize UART
+   * Ports: 16+7 (TX+RX)
+   * Baudrate: 2 million
+   */
+  bl_uart_init(0, 16, 7, 255, 255, 2 * 1000 * 1000);
+  printf("Hello world!\r\n");
+
+  hal_hwtimer_init();
+
+  struct config_74hc595 sr_config = {
+      .with_output_enable = 1,
+      .pin_output_enable = 0,
+      .pin_data_signal = 1,
+      .pin_shift_clock = 2,
+      .pin_store_clock = 3,
+      .with_master_reset = 1,
+      .pin_master_reset = 4,
+      .number_of_registers = 2};
+
+  configure_74hc595(&sr_config);
+
+  for (uint8_t i = 0; i < 129; i++)
+  {
+    bl_timer_delay_us(1 * 1000 * 1000);
+    store_data_74hc595(&sr_config, i);
+  }
+}

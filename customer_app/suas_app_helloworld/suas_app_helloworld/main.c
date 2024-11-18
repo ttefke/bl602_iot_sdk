e84e829be846327204f7324caa1acda24a373655
Tobias Tefke <tobias@tefke.name>
2024-11-18 10:39:09 +0100

    Add SUAS hello world application

---
+++ customer_app/suas_app_helloworld/suas_app_helloworld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <bl_uart.h>
+
+void bfl_main(void)
+{
+  /* Initialize UART
+   * Ports: 16+7 (TX+RX)
+   * Baudrate: 2 million
+   */
+  bl_uart_init(0, 16, 7, 255, 255, 2 * 1000 * 1000);
+  printf("Hello world!\r\n");
+}

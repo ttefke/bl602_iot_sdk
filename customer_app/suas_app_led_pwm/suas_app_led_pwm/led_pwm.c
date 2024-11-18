0e001a6128261ccef2ce0a4d267770a36cb9140f
Tobias Tefke <tobias@tefke.name>
2024-11-18 10:44:07 +0100

    Add SUAS LED PWM app

---
+++ customer_app/suas_app_led_pwm/suas_app_led_pwm/led_pwm.c
@@ -0,0 +1,53 @@
+// FreeRTOS
+#include <FreeRTOS.h>
+#include <task.h>
+
+// Input/output
+#include <stdio.h>
+
+// PWM library
+#include <bl_pwm.h>
+
+// Define LED pins
+#define LED_R_PIN 17
+#define LED_R_PWM_CHANNEL 2
+
+// frequency
+#define PWM_FREQUENCY 2048
+#define PWM_FREQUENCY_DIVIDER 4
+
+/* LED task */
+void task_led_pwm(void *pvParameters)
+{
+  printf("LED PWM task started\r\n");
+  
+  /* Usage: bl_pwm_init(PWM channel -> see manual p. 27, pin, frequency (must be between 2,000 and 800,000)) */
+  bl_pwm_init(LED_R_PWM_CHANNEL, LED_R_PIN, PWM_FREQUENCY);
+  
+  // Set frequency to 512Hz: set 4 as divider value -> real frequency = 2048/4 = 512Hz
+  PWM_Channel_Set_Div(LED_R_PWM_CHANNEL, PWM_FREQUENCY_DIVIDER);
+  
+  /* Set duty cycle
+     duty cycle must be between 0 and 100
+  */
+  bl_pwm_set_duty(LED_R_PWM_CHANNEL, 1);
+
+  // Start PWM operations
+  bl_pwm_start(LED_R_PWM_CHANNEL);
+  
+  while (1) {
+    for (short value = 0; value < 100; value++) {          
+      /* Set duty cycle
+        duty cycle must be between 0 and 100
+      */
+      bl_pwm_set_duty(LED_R_PWM_CHANNEL, value);
+         
+      /* Wait for 20ms */
+      vTaskDelay(20 / portTICK_PERIOD_MS);
+    }
+  }
+  
+  
+  // should never happen but would delete the task and free allocated resources
+  vTaskDelete(NULL);
+}
---
+++ customer_app/suas_app_led_pwm/suas_app_led_pwm/main.c
@@ -0,0 +1,61 @@
+// FreeRTOS includes
+#include <FreeRTOS.h>
+#include <task.h>
+
+// Input/Output
+#include <stdio.h>
+
+// UART library
+#include <bl_uart.h>
+
+/* Define heap regions */
+extern uint8_t _heap_start;
+extern uint8_t _heap_size;
+extern uint8_t _heap_wifi_start;
+extern uint8_t _heap_wifi_size;
+
+static HeapRegion_t xHeapRegions[] =
+{
+  { &_heap_start, (unsigned int) &_heap_size},
+  { &_heap_wifi_start, (unsigned int) &_heap_wifi_size },
+  { NULL, 0},
+  { NULL, 0}
+};
+
+/* Size of the stack for the task */
+#define LED_STACK_SIZE 512
+
+void bfl_main(void)
+{
+  /* Initialize UART
+   * Ports: 16+7 (TX+RX)
+   * Baudrate: 2 million
+   */
+  bl_uart_init(0, 16, 7, 255, 255, 2 * 1000 * 1000);
+  
+   /* (Re)define Heap */
+  vPortDefineHeapRegions(xHeapRegions);
+  
+  /* Set up LED task */
+  static StackType_t led_stack_pwm[LED_STACK_SIZE];
+  static StaticTask_t led_task_pwm;
+  
+  /* Start up LED task */
+  extern void task_led_pwm(void *pvParameters); 
+  /* This function is defined in led.c but add prototype here so that the compiler knows we define this in another file.
+  You could also use a header file for this */
+
+  /* Create the task */
+  xTaskCreateStatic(
+    task_led_pwm, /* name of the function implementing the task -> defined in led_pwm.c */
+    (char*)"led pwm", /* human readable name of the task */
+    LED_STACK_SIZE, /* Stack size */
+    NULL, /* parameters for the function -> not required here */
+    15, /* Task priority */
+    led_stack_pwm, /* Stack to use for the task */
+    &led_task_pwm /* Task handle (could be referenced in API calls later, e.g. for changing its priority )*/    
+  );
+  
+  /* Also start the task (task creation only creates the task control block) */
+  vTaskStartScheduler();
+}

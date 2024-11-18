662dc72fbd91754302b99aa1419629522ed7910b
Tobias Tefke <tobias@tefke.name>
2024-11-18 10:33:51 +0100

    Add mbedTLS functions

--- components/security/mbedtls_new/include/mbedtls/bl602_functions.h
+++ components/security/mbedtls_new/include/mbedtls/bl602_functions.h
@@ -5,7 +5,9 @@
 #include <stdint.h>
 
 void mbedtls_platform_zeroize(void *buf, size_t len);
+void mbedtls_zeroize_and_free(void *buf, size_t len);
 uint32_t mbedtls_get_unaligned_uint32(const void *p);
 void mbedtls_put_unaligned_uint32(void *p, uint32_t x);
+void mbedtls_xor(unsigned char *r, const unsigned char *a, const unsigned char *b, size_t n);
 
 #endif

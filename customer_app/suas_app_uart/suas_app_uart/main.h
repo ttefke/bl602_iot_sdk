b28be8d6acf0f7d788a0724f0ab761ad326011d5
Tobias Tefke <tobias@tefke.name>
2025-05-03 21:12:40 +0200

    Add UART sample project

---
+++ customer_app/suas_app_uart/suas_app_uart/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+// Stack size
+#define SENSOR_STACK_SIZE 256
+
+#endif /* __MAIN_H */
---
+++ customer_app/suas_app_uart/suas_app_uart/mmWave.h
@@ -0,0 +1,8 @@
+#ifndef __MMWAVE_H
+#define __MMWAVE_H
+
+#define DATA_ARRAY_LENGTH 2
+
+void read_sensor(void *pvParameters);
+
+#endif

fcc460652c555d5cf32959d1b01401c889e8f1ed
Tobias Tefke <tobias@tefke.name>
2025-05-25 21:48:08 +0200

    suas blemesh example: update node configuration

--- customer_app/suas_app_ble_mesh/suas_app_ble_mesh/node.c
+++ customer_app/suas_app_ble_mesh/suas_app_ble_mesh/node.c
@@ -23,18 +23,10 @@
 
 // Server configuration
 struct bt_mesh_cfg_srv cfg_srv = {
-	.relay = BT_MESH_RELAY_DISABLED,
-	.beacon = BT_MESH_BEACON_ENABLED,
-#if defined(CONFIG_BT_MESH_FRIEND)
-	.frnd = BT_MESH_FRIEND_ENABLED,
-#else
+	.relay = BT_MESH_RELAY_ENABLED,
+	.beacon = BT_MESH_BEACON_DISABLED,
 	.frnd = BT_MESH_FRIEND_NOT_SUPPORTED,
-#endif
-#if defined(CONFIG_BT_MESH_GATT_PROXY)
-	.gatt_proxy = BT_MESH_GATT_PROXY_ENABLED,
-#else
 	.gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
-#endif
 	.default_ttl = 3,
 
 	/* 3 transmissions with 20ms interval */

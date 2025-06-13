39922da65386db4c6fca638170d4c65aed8beb58
Tobias Tefke <tobias@tefke.name>
2025-06-13 18:08:52 +0200

    Add WLAN AP/STA example

---
+++ customer_app/suas_app_wlan_ap_sta/suas_app_wlan_ap_sta/wifi.h
@@ -0,0 +1,16 @@
+#ifndef __WIFI_H
+#define __WIFI_H
+
+// Warning: do not use hardcoded credentials in production code!
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PW "YOUR_PASSWORD"
+
+// Wifi modes
+enum app_wifi_role {
+    AP = 1,
+    STA
+};
+
+// Function prototypes
+void task_wifi(void *pvParameters);
+#endif

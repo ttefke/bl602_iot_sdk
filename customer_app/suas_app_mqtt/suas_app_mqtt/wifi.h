24afd27f11e5cba888def1d140984d474be0f2d8
Tobias Tefke <tobias@tefke.name>
2025-06-22 18:53:12 +0200

    Add MQTT example

---
+++ customer_app/suas_app_mqtt/suas_app_mqtt/wifi.h
@@ -0,0 +1,11 @@
+#ifndef __WIFI_H
+#define __WIFI_H
+
+// Credentials
+// Do not hardcode credentials in production code
+#define WIFI_SSID "SUASecLab_IoT_Gateway"
+#define WIFI_PW "2aBkY5k5emEjMgxKqdTj"
+
+// Function prototypes
+void task_wifi(void *pvParameters);
+#endif

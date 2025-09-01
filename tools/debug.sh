dea05d9d3e1bc76bde9a679ca05bc591b399c49d
Tobias Tefke <tobias@tefke.name>
2025-09-01 12:22:42 +0200

    Add script to start openOCD, GDB and attach GDB to openOCD

---
+++ tools/debug.sh
@@ -0,0 +1,29 @@
+if ! command -v gnome-terminal >/dev/null 2>&1
+then
+    echo "gnome-terminal could not be found, can not run program"
+    exit 1
+fi
+
+path=$(pwd)
+wd=$(basename "$path")
+upd=$(basename "$(dirname "$path")")
+
+if [[ "$upd" != "customer_app" ]]; then
+    echo "Script must be started from the application's source code root directory"
+    exit 1
+fi
+
+echo "Starting openOCD"
+gnome-terminal --  bash -c "$path/../../tools/openocd/bin/openocd -f $path/../../tools/openocd/openocd.cfg; exec bash"
+
+echo "Creating GDB configuration"
+
+cat > /tmp/gdb_tgt.cfg << EOF
+target extended-remote :3333
+EOF
+
+
+cat $(pwd)/../../tools/debug/602.init /tmp/gdb_tgt.cfg > /tmp/602.init
+
+echo "Starting GDB"
+gnome-terminal -- bash -c "$path/../../toolchain/riscv/Linux/bin/riscv64-unknown-elf-gdb -x /tmp/602.init --se=$path/build_out/$wd.elf; exec bash"

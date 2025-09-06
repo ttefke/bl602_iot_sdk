5979c8be43ee73bb9f09d2cd40baff6f981fc3e9
Tobias Tefke <tobias@tefke.name>
2025-09-06 12:05:16 +0200

    Add script to ease usage of objdump tool

---
+++ tools/objdump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path=$(pwd)
+wd=$(basename "$path")
+upd=$(basename "$(dirname "$path")")
+
+if [[ "$upd" != "customer_app" ]]; then
+    echo "Script must be started from the application's source code root directory"
+    exit 1
+fi
+
+../../toolchain/riscv/Linux/bin/riscv64-unknown-elf-objdump build_out/$wd.elf -x -s -S --disassemble --demangle --line-numbers --wide > $wd.S 2>&1

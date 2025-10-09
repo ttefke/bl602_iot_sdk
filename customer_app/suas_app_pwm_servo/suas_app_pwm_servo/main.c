55d0ae6b7a442f480a15d214cb446c815c5297a8
Tobias Tefke <tobias@tefke.name>
2025-10-09 15:07:03 +0200

    Add servo example

---
+++ customer_app/suas_app_pwm_servo/suas_app_pwm_servo/main.c
@@ -0,0 +1,35 @@
+// FreeRTOS includes
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* Size of the stack for the task */
+#define SERVO_STACK_SIZE 512
+
+void bfl_main(void)
+{  
+  /* Set up servo task */
+  static StackType_t servo_stack_pwm[SERVO_STACK_SIZE];
+  static StaticTask_t servo_task_pwm;
+
+  // Initialize system
+  vInitializeBL602();
+  
+  /* Start up servo task */
+  extern void task_servo_pwm(void *pvParameters); 
+  /* This function is defined in servo_pwm.c but add prototype here so that the compiler knows we define this in another file.
+  You could also use a header file for this */
+
+  /* Create the task */
+  xTaskCreateStatic(
+    task_servo_pwm, /* name of the function implementing the task -> defined in servo_pwm.c */
+    (char*)"servo pwm", /* human readable name of the task */
+    SERVO_STACK_SIZE, /* Stack size */
+    NULL, /* parameters for the function -> not required here */
+    15, /* Task priority */
+    servo_stack_pwm, /* Stack to use for the task */
+    &servo_task_pwm /* Task handle (could be referenced in API calls later, e.g. for changing its priority )*/    
+  );
+  
+  /* Also start the task (task creation only creates the task control block) */
+  vTaskStartScheduler();
+}

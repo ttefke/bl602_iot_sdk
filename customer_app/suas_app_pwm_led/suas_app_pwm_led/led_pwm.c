cfc185802d11dd7184266aa75e3bc483d0363fe9
Tobias Tefke <tobias@tefke.name>
2025-10-09 12:15:25 +0200

    Use suas pwm driver for pwm example

---
+++ customer_app/suas_app_pwm_led/suas_app_pwm_led/led_pwm.c
@@ -0,0 +1,44 @@
+// FreeRTOS
+#include <FreeRTOS.h>
+#include <task.h>
+
+// Input/output
+#include <stdio.h>
+
+// PWM library
+#include <suas_pwm.h>
+
+/* LED task */
+void task_led_pwm([[gnu::unused]] void *pvParameters)
+{
+  printf("LED PWM task started\r\n");
+
+  /* 1. Create configuration for the PWM library */
+  suas_pwm_conf_t conf = {
+    .pin = 17, // Pin of the GPIO pin to use
+    .frequency = 2048, // Frequency to set (must be between 2,000 and 800,000)
+    .frequency_divider = 4, // Set a frequency divider.
+                            // The actual frequency will be frequency/frequency_divider = 2048/4 = 512
+    .duty = 1.0f, // Set initial duty cycle (must be between 0 and 100)
+  };
+
+  /* 2. Initialize PWM */
+  suas_pwm_init(&conf);
+  
+  /* 3. Start PWM */
+  suas_pwm_start(&conf);
+  
+  while (1) {
+    for (short value = 0; value < 100; value++) {       
+      /* Change duty cycle */   
+      conf.duty = value;
+      suas_pwm_set_duty(&conf);
+         
+      /* Wait for 20ms */
+      vTaskDelay(pdMS_TO_TICKS(20));
+    }
+  }
+  
+  // Should never happen but would delete the task and free allocated resources
+  vTaskDelete(NULL);
+}
